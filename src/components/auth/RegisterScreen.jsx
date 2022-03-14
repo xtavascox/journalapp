d5915eb9fe3a7beaf3cc6e3b5dbf39a66859c8e3
Gustavo Esteven Fajardo Cruz <gfajardo1997@gmail.com>
2022-03-14 00:34:52 -0500

    progress

--- src/components/auth/RegisterScreen.jsx
+++ src/components/auth/RegisterScreen.jsx
@@ -6,10 +6,11 @@ export const RegisterScreen = () => {
     <>
       <h3 className='auth__title mb-5'>Register</h3>
       <form>
-        
+        <input type='text' placeholder='Name' name='name' className='auth__input' autoComplete='off' />
         <input type="text" placeholder='Email' name='email' className='auth__input' />
         <input type="password" placeholder='Password' name='password' className='auth__input' />
-        <button type='submit' className='btn btn-primary btn-block mb-5'> Login</button>
+        <input type="password" placeholder='Confirm Password' name='password2' className='auth__input' />
+        <button type='submit' className='btn btn-primary btn-block mb-5'> Create Account</button>
 
         <Link to='/auth/login' className='link'>Already registered?</Link>
       </form>
--- src/components/journal/JournalScreen.jsx
+++ src/components/journal/JournalScreen.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 export const JournalScreen = () => {
   return (
-    <div>JournalScreen</div>
+    <div className='journal__main-content'>
+      
+      <Sidebar/>
+
+      <main>
+        <h1>Main Content</h1>
+      </main>
+
+    </div>
   )
 }
 
